5ba48b29f5397e52bca9f95568b80a3185637e05
Vladislav Nesterov <nesterovvlad100@gmail.com>
2021-11-06 16:58:53 +0300

    Boze neujeli eto vse?

--- core/test/CircleTest.scala
+++ core/test/CircleTest.scala
@@ -1,22 +1,22 @@
 
-//import org.scalatest._
-//import org.scalatest.flatspec._
-//import org.scalatest.matchers._
+import org.scalatest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
 
-//class CircleTest extends AnyFlatSpec with should.Matchers {
+class CircleTest extends AnyFlatSpec with should.Matchers {
 
-// def victim() = new CircleRound
+ def victim() = new CircleRound
 
-//"CircleRound" should "calculate area for given Circle data" in {
-//  val rd = CircleSquare(2)
+"CircleRound" should "calculate area for given Circle data" in {
+  val rd = CircleSquare(2)
 
-//  victim().area(rd) should be (12.56)
-// }
+    victim().area(rd) should be (12.56)
+ }
 
-//it should "return zero if Round is zero" in {
-// val rd = CircleSquare(0)
+it should "return zero if Round is zero" in {
+ val rd = CircleSquare(0)
 
-//victim().area(rd) should be (0)
-// }
+victim().area(rd) should be (0)
+ }
 
-//}
+}
